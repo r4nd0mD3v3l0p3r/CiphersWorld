a3c2433b3c9ff5ad8025dcd10d6a2f87f2b7b133
r4nd0md3v3l0p3r <lorenzo.chiovini@hotmail.it>
2019-12-26 16:27:34 +0100

    better drawer layout

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -4,27 +4,39 @@ import {Container, Content, List, ListItem, Text} from 'native-base'
 const routes = [
     {name: 'Home', key: 'home'},
     {name: 'About', key: 'about'},
+]
+
+const ciphers = [
     {name: 'Atbash', key: 'atbash'},
     {name: 'Caesar', key: 'caesar'},
     {name: 'Vigenere', key: 'vigenere'},
 ]
+
 export const SideBar = ({navigation}) => {
+    const listItem = item =>
+        (<ListItem
+            key={item.key}
+            button
+            onPress={() => navigation.navigate(item.name)}>
+            <Text>{item.name}</Text>
+        </ListItem>)
+
     return (
         <Container>
             <Content>
-                <List
-                    dataArray={routes}
-                    renderRow={data => {
-                        return (
-                            <ListItem
-                                key={data.key}
-                                button
-                                onPress={() => navigation.navigate(data.name)}>
-                                <Text>{data.name}</Text>
-                            </ListItem>
-                        )
-                    }}
-                />
+                <List>
+                    <ListItem itemHeader first>
+                        <Text>GENERAL</Text>
+                    </ListItem>
+                    {routes.map(x => listItem(x))}
+                </List>
+
+                <List>
+                    <ListItem itemHeader first>
+                        <Text>CIPHERS</Text>
+                    </ListItem>
+                    {ciphers.map(x => listItem(x))}
+                </List>
             </Content>
         </Container>
     )
